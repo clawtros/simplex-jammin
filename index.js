9cdbc69a490947f065a2a2821cbb86658d537e17
Adam Benzan <adam.benzan@gmail.com>
2018-01-29 09:44:30 -0500

    play with other canvas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const OpenSimplex = require("open-simplex-noise").default;
 const simp = new OpenSimplex(new Date());
-const NUM_POINTS = 1e3;
+const NUM_POINTS = 4e3;
 
 function generatePoints(w, h) {
   const r = [],
@@ -14,29 +14,41 @@ function generatePoints(w, h) {
 
 function main() {
   var canvas = document.createElement("canvas"),
+      otherCanvas = document.createElement("canvas"),
       context = canvas.getContext("2d"),
+      otherContext = otherCanvas.getContext("2d"),
       width = window.innerWidth,
       height = window.innerHeight,
       points = generatePoints(width / 2, height / 2),
-      speed = 2,
-      scale = 500;
+      speed = 5,
+      scale = 100;
   
   canvas.width = width;
   canvas.height = height;
+  otherCanvas.width = width / scale;
+  otherCanvas.height = height / scale;
+  var imageData = otherContext.createImageData(width / scale, height / scale);
   var z = 0;
 
   function draw() {
     //context.fillStyle = "rgba(255, 255, 255, 0.5)";
     //context.fillRect(0, 0, width, height)
-    context.fillStyle = "rgba(0, 0, 0, 0.15)";
+    canvas.width = width;
+    context.fillStyle = "rgba(0, 0, 0, 0.75)";
     for (let i = 0; i < NUM_POINTS; i++) {
       let [x, y] = points[i],
-          noise = simp.noise3D(x / scale, y / scale, z) * 10,
-          [nx, ny] = [x + Math.sin(noise) * speed , y + (Math.cos(noise) ) * speed],
-          s = 2;
+          [sx, sy] = points[i].map(p=>p / scale),
+          noise = simp.noise3D(sx, sy, z) * 15,
+          [nx, ny] = [x + Math.sin(noise) * speed, y + (Math.cos(noise) ) * speed].map(n=>n + (Math.random() - 0.5) * 5),
+          s = 1,
+          idx = (sx + sy) * 4;
       
       context.fillRect(x, y, s, s);
-
+      imageData[sx] = noise * 255;
+      imageData[sx + 1] = 0;
+      imageData[sx + 2] = 255;
+      imageData[sx + 3] = 255;
+      
       if (nx > -100 && nx < canvas.width + 100 && nx > -100 && ny < canvas.height + 100) {
         points[i] = [nx, ny]
       } else {
@@ -44,17 +56,16 @@ function main() {
       }
       
     }
+    context.putImageData(imageData, 0, 0);
   }
   
-  canvas.style.position = "absolute";
-  canvas.style.top = 0;
-  canvas.style.left = 0;
-  
   document.body.appendChild(canvas);
+
+  document.body.appendChild(otherCanvas);  
   function animate() {
     requestAnimationFrame(animate);
     draw();
-    z += 0.0125;
+    z += 0.0025;
   }
   animate();
 }
